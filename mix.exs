bc7c8a13a86ff43c437f908d156eb169079bf5e3
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-10-21 21:53:06 -0400

    Fix dependency issues; change absinthe to 1.0

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Roots.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.0.0"}
     ]
   end
 
