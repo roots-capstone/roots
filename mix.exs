89903608c1b443a522a0bedb023b2acc7a2f86a1
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-10-25 18:22:31 -0600

    Updata mix.exs deps. Add: absinthe_phoenix, absinthe_plug, guardian

--- mix.exs
+++ mix.exs
@@ -43,7 +43,10 @@ defmodule Roots.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:absinthe, "~> 1.0.0"}
+      {:absinthe, "~> 1.5.0"},
+      {:absinthe_phoenix, "~> 2.0.0"},
+      {:absinthe_plug, "~> 1.5.0"},
+      {:guardian, "~> 1.0"},
     ]
   end
 
