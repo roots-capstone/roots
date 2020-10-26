da029e190c6fd0e10987ac2cb798fd089b97f15e
“Neeru <neeram85@gmail.com>
2020-10-26 16:09:21 -0600

    Write get all user cookbooks query

--- lib/roots_web/schema/user_types.ex
+++ lib/roots_web/schema/user_types.ex
@@ -14,14 +14,18 @@ defmodule RootsWeb.Schema.UserTypes do
     field :cookbooks, list_of(:cookbook), resolve: dataloader(Data)
   end
 
-  object :user_mutations do
+  object :user_queries do
+    @desc "Get all user cookbooks"
+    field :all_cookbooks, non_null(list_of(non_null(:cookbook)))
+  end
 
+  object :user_mutations do
     @desc "Create User"
     field :create_user, type: :user do
       arg(:name, non_null(:string))
       arg(:email, non_null(:string))
 
-      resolve &Resolvers.UserResolver.create_user/3
+      resolve(&Resolvers.UserResolver.create_user/3)
     end
   end
 end
