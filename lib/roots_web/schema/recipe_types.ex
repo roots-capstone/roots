5b0b27e56e9c6df5df8d3a89dbf0c8874b1459a8
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-10-26 17:15:46 -0400

    Create RecipeTypes; create :recipe object and :recipe_mutations

---
+++ lib/roots_web/schema/recipe_types.ex
@@ -0,0 +1,31 @@
+defmodule RootsWeb.Schema.RecipeTypes do
+  use Absinthe.Schema.Notation
+
+  import Absinthe.Resolution.Helpers, only: [dataloader: 1]
+
+  alias RootsWeb.{Data, Resolvers}
+
+  @desc "A recipe"
+  object :recipe do
+    field :id, :id
+    field :description, :string
+    field :instructions, :string
+    field :title, :string
+    field :author, :string
+
+    field :cookbook, :cookbook, resolve: dataloader(Data)
+    field :ingredients, list_of(:ingredient), resolve: dataloader(Data)
+  end
+
+  object :recipe_mutations do
+    @desc "Create Recipe"
+    field :create_recipe, type: :recipe do
+      arg(:description, non_null(:string))
+      arg(:instructions, non_null(:string))
+      arg(:title, non_null(:string))
+      arg(:author, non_null(:string))
+
+      resolve &Resolvers.RecipeResolver.create_recipe/3
+    end
+  end
+end
