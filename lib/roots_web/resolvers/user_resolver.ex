91347c3f7a15f4f0635700bc14ccdd562631a1dd
“Neeru <neeram85@gmail.com>
2020-10-26 16:03:45 -0600

    Remove commented out code from user resolver

--- lib/roots_web/resolvers/user_resolver.ex
+++ lib/roots_web/resolvers/user_resolver.ex
@@ -2,16 +2,12 @@ defmodule RootsWeb.Resolvers.UserResolver do
   alias Roots.User
 
   def create_user(_parent, args, _resolution) do
-
     case User.create(args) do
       {:ok, user} ->
         {:ok, user}
-        _error ->
+
+      _error ->
         {:error, "Could not create user"}
-    # |> case do
-    #   {:ok, user} ->
-    #   {:error, 'Unable to create new user'}
-    #     # {:error, extract_error_msg(changeset)}
     end
   end
 end
