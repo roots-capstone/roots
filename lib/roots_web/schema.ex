a2cb32eaffee33d71018d0f8e43fe1fecf9c335a
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-10-25 18:54:41 -0600

    Fix Typos

--- lib/roots_web/schema.ex
+++ lib/roots_web/schema.ex
@@ -1,7 +1,9 @@
-defmodule Roots.Schema do
-  use Absinth.Schema
+defmodule RootsWeb.Schema do
+  use Absinthe.Schema
 
-  import_types(Absinth.Type.Custom)
+  alias RootsWeb.Data
+
+  import_types(Absinthe.Type.Custom)
 
   import_types(RootsWeb.Schema.UserTypes)
   # ...other models' types added here
@@ -14,5 +16,21 @@ defmodule Roots.Schema do
   mutation do
     import_fields(:user_mutations)
     # ... other models' mutations added here
+  end
+
+  def context(ctx) do
+    loader =
+      Dataloader.new()
+      |> Dataloader.add_source(Data, Data.data())
+
+    Map.put(ctx, :loader, loader)
+  end
+
+  def plugins do
+    [Absinthe.Middleware.Dataloader] ++ Absinthe.Plugin.defaults()
+  end
+
+  def middleware(middleware, _field, _object) do
+    [NewRelic.Absinthe.Middleware] ++ middleware
   end
 end
