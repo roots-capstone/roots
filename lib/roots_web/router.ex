e4bafbeed8d46657995f240acb25ec9c0e1a7fe7
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-10-25 18:54:21 -0600

    Add notes

--- lib/roots_web/router.ex
+++ lib/roots_web/router.ex
@@ -1,27 +1,16 @@
 defmodule RootsWeb.Router do
-  use RootsWeb, :router
+  use Phoenix.Router
 
   pipeline :api do
+    plug RootsWeb.Context
     plug :accepts, ["json"]
   end
 
-  scope "/api", RootsWeb do
+  scope "/" do
     pipe_through :api
-  end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-      live_dashboard "/dashboard", metrics: RootsWeb.Telemetry
-    end
+    # PIPELINE ?
+    forward "/graphiql", Absinthe.Plug.GraphiQL, schema: RootsWeb.Schema
+    # PIPELINE ?
+    forward "/", Absinthe.Plug, schema: RootsWeb.Schema
   end
 end
