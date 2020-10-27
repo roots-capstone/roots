4da6df31e04a2c9e2d74e60d7b103027b55cdf78
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-10-27 09:22:42 -0600

    Update seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,69 +10,68 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+
+## TO CLEAR OUT THE DATABASE SO THERE ARE NO DUPLICATES
+## AND THEN SEED THE DATABASE
+## RUN  $ mix ecto.reset
+
 alias Roots.Repo
+alias Repo.{User, Cookbook}
 
-user_data = [
-  %{
-    name: "Ryan",
-    email: "ryan@roots.com"
-  },
-  %{
-    name: "Neeru",
-    email: "neeru@roots.com"
-  },
-  %{
-    name: "michaelAlex",
-    email: "michaelAlex@roots.com"
-  },
-  %{
-    name: "Jesseye",
-    email: "jesseye@roots.com"
-  },
-  %{
-    name: "Tyler",
-    email: "tyler@roots.com"
-  },
-  %{
-    name: "Horacio",
-    email: "horacio@roots.com"
-  }
-]
 
-Enum.each(user_data, fn(data) ->
-  Repo.insert!(%Roots.User{name: data.name, email: data.email})
-end)
+ryan = Repo.insert!(%Roots.User{
+  name: "Ryan",
+  email: "ryan@roots.com"
+})
+neeru = Repo.insert!(%Roots.User{
+  name: "Neeru",
+  email: "neeru@roots.com"
+})
+michaelAlex = Repo.insert!(%Roots.User{
+  name: "michaelAlex",
+  email: "michaelAlex@roots.com"
+})
+jesseye = Repo.insert!(%Roots.User{
+  name: "Jesseye",
+  email: "jesseye@roots.com"
+})
+tyler = Repo.insert!(%Roots.User{
+  name: "Tyler",
+  email: "tyler@roots.com"
+})
+horacio = Repo.insert!(%Roots.User{
+  name: "Horacio",
+  email: "horacio@roots.com"
+})
 
-ryan = Repo.insert!(%Roots.User{name: "Ryan", email: "user_ryan@roots.com"})
 
-cookbook_data = [
-  %{
-    title: "Ryan's Cookbook",
-    author: "ryan@roots.com",
-    user: ryan
-  },
-  %{
-    title: "Persian Food",
-    author: "Ryan Laleh",
-    user: ryan
-  },
-  %{
-    title: "Mexican Food",
-    author: "Ryan Laleh",
-    user: ryan
-  },
-  %{
-    title: "Japanese Food",
-    author: "Ryan Laleh",
-    user: ryan
-  },
-  %{
-    title: "My Favorite Food",
-    author: "Ryan Laleh",
-    user: ryan
-  }
-]
-
-Enum.each(cookbook_data, fn(data) ->
-  Repo.insert!(%Roots.Cookbook{title: data.title, author: data.author, user: data.user})
-end)
+Repo.insert!(%Roots.Cookbook{
+  title: "Ryan's Cookbook",
+  author: "Ryan",
+  user: ryan
+})
+Repo.insert!(%Roots.Cookbook{
+  title: "Neeru's Cookbook",
+  author: "Neeru",
+  user: neeru
+})
+Repo.insert!(%Roots.Cookbook{
+  title: "MichaelAlex's Cookbook",
+  author: "MichaelAlex",
+  user: michaelAlex
+})
+Repo.insert!(%Roots.Cookbook{
+  title: "Jesseye's Cookbook",
+  author: "Jesseye",
+  user: jesseye
+})
+Repo.insert!(%Roots.Cookbook{
+  title: "Tyler's Cookbook",
+  author: "Tyler",
+  user: tyler
+})
+Repo.insert!(%Roots.Cookbook{
+  title: "Horacio's Cookbook",
+  author: "Horacio",
+  user: horacio
+})
