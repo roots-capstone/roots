75e3e3ecf50c3d905e31931a0f45dbfcfb4ef51b
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-10-27 11:26:03 -0400

    Create recipe_resolver.ex

---
+++ lib/roots_web/resolvers/recipe_resolver.ex
@@ -0,0 +1,13 @@
+defmodule RootsWeb.Resolvers.RecipeResolver do
+  alias Roots.Recipe
+
+  def create_recipe(_parent, args, _resolution) do
+
+    case Recipe.create(args) do
+      {:ok, recipe} ->
+        {:ok, recipe}
+        _error ->
+        {:error, "Could not create user"}
+    end
+  end
+end
