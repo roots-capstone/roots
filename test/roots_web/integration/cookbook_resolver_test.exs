685b9a4683a9739e0679dda6e3f89a30c74fd461
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-10-27 14:32:47 -0600

    Create cookbook resolver integration test function list
    Co-Authored-By: Michael Alex <53200401+mlynch5187@users.noreply.github.com>

---
+++ test/roots_web/integration/cookbook_resolver_test.exs
@@ -0,0 +1,48 @@
+defmodule RootsWeb.Integration.CookbookResolverTest do
+  use RootsWeb.ConnCase
+  alias Roots.AbsintheHelpers
+  alias Roots.Repo
+  require IEx
+
+  describe "#list" do
+    test "it returns cookbooks" do
+      user = Repo.insert!(%Roots.User{
+        name: "User",
+        email: "user@roots.com"
+      })
+      usersCookbook = Repo.insert!(%Roots.Cookbook{
+        title: "User's Cookbook",
+        author: "Author Name",
+        user: user
+      })
+      persianFood = Repo.insert!(%Roots.Cookbook{
+        title: "Persion Food",
+        author: "Author Name",
+        user: user
+      })
+
+      query = """
+      {
+        getCookbooks {
+          id
+          title
+          author
+          user {
+            id
+            name
+            email
+          }
+        }
+      }
+      """
+
+      res =
+        build_conn()
+        |> post("/graphql", AbsintheHelpers.query_skeleton(query, "getCookbooks"))
+
+      cookbooks = json_response(res, 200)["data"]["getCookbooks"]
+      assert List.first(cookbooks)["id"] == to_string(persianFood.id)
+      assert List.last(cookbooks)["id"] == to_string(usersCookbook.id)
+    end
+  end
+end
