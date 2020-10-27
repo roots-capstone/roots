2b1553aa0191ae9fe017bac795633244b8111bb9
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-10-27 13:07:06 -0600

    Still trying to work out the tests for cookbook type

--- test/roots/cookbook_test.exs
+++ test/roots/cookbook_test.exs
@@ -1,29 +1,32 @@
-# defmodule Roots.CookbookTest do
-#   use RootsWeb.ConnCase
-#
-#   alias Roots.Cookbook
-#   alias Roots.Repo
-#
-#   describe "#all" do
-#     test "it finds all cookbooks" do
-#       user = Repo.insert!(%Roots.User{
-#         name: "User",
-#         email: "user@roots.com"
-#       })
-#       usersCookbook = Repo.insert!(%Roots.Cookbook{
-#         title: "User's Cookbook",
-#         author: "Author Name",
-#         user: user
-#       })
-#       persianFood = Repo.insert!(%Roots.Cookbook{
-#         title: "Persion Food",
-#         author: "Author Name",
-#         user: user
-#       })
-#       results = Cookbook.all()
-#       assert length(results) == 2
-#       assert List.first(results).id == usersCookbook.id
-#       assert List.last(results).id == persianFood.id
-#     end
-#   end
-# end
+defmodule Roots.CookbookTest do
+  use RootsWeb.ConnCase
+
+  alias Roots.Cookbook
+  alias Roots.Repo
+
+  describe "#all" do
+    test "it finds all cookbooks" do
+      # user = Repo.insert!(%Roots.User{
+      #   name: "User",
+      #   email: "user@roots.com"
+      # })
+      # usersCookbook = Repo.insert!(%Roots.Cookbook{
+      #   title: "User's Cookbook",
+      #   author: "Author Name",
+      #   user: user
+      # })
+      # persianFood = Repo.insert!(%Roots.Cookbook{
+      #   title: "Persion Food",
+      #   author: "Author Name",
+      #   user: user
+      # })
+
+      userCookbook = insert(:cookbook)
+      persianFood  = insert(:cookbook)
+      results = Cookbook.all()
+      assert length(results) == 2
+      assert List.first(results).id == usersCookbook.id
+      assert List.last(results).id == persianFood.id
+    end
+  end
+end
