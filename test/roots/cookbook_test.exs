2111e6b330bb294d8f2474ce79550087dcf34d8f
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-10-27 16:20:16 -0600

    Add tests for cookbook model: #create, #find

--- test/roots/cookbook_test.exs
+++ test/roots/cookbook_test.exs
@@ -5,6 +5,17 @@ defmodule Roots.CookbookTest do
   alias Roots.Cookbook
   alias Roots.Repo
 
+
+  @valid_attrs %{title: "Cookbook Name", author: "User Name"}
+
+  describe "#create" do
+    test "it can create a new cookbook" do
+      {:ok, cookbook} = Cookbook.create(@valid_attrs)
+      assert cookbook.title == "Cookbook Name"
+      assert cookbook.author == "User Name"
+    end
+  end
+
   describe "#all" do
     test "it finds all cookbooks" do
       user = Repo.insert!(%Roots.User{
@@ -26,6 +37,23 @@ defmodule Roots.CookbookTest do
       assert length(results) == 2
       assert List.first(results).id == persianFood.id
       assert List.last(results).id == usersCookbook.id
+    end
+  end
+
+  describe "#find" do
+    test "it can find a cookbook by id" do
+      user = Repo.insert!(%Roots.User{
+        name: "User",
+        email: "user@roots.com"
+      })
+      usersCookbook = Repo.insert!(%Roots.Cookbook{
+        title: "User's Cookbook",
+        author: "Author Name",
+        user: user
+      })
+      cookbook_found = Cookbook.find(usersCookbook.id)
+      assert cookbook_found.title == "User's Cookbook"
+      assert cookbook_found.author == "Author Name"
     end
   end
 end
