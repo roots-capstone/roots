6864ab9706e78524696e4841fda56c92384d5e9d
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-10-27 14:15:28 -0600

    Add cookbook resolver tests for list and show function
    Co-Authored-By: Michael Alex <53200401+mlynch5187@users.noreply.github.com>

--- test/roots/cookbook_test.exs
+++ test/roots/cookbook_test.exs
@@ -1,32 +1,31 @@
 defmodule Roots.CookbookTest do
   use RootsWeb.ConnCase
+  require IEx
 
   alias Roots.Cookbook
   alias Roots.Repo
 
   describe "#all" do
     test "it finds all cookbooks" do
-      # user = Repo.insert!(%Roots.User{
-      #   name: "User",
-      #   email: "user@roots.com"
-      # })
-      # usersCookbook = Repo.insert!(%Roots.Cookbook{
-      #   title: "User's Cookbook",
-      #   author: "Author Name",
-      #   user: user
-      # })
-      # persianFood = Repo.insert!(%Roots.Cookbook{
-      #   title: "Persion Food",
-      #   author: "Author Name",
-      #   user: user
-      # })
+      user = Repo.insert!(%Roots.User{
+        name: "User",
+        email: "user@roots.com"
+      })
+      usersCookbook = Repo.insert!(%Roots.Cookbook{
+        title: "User's Cookbook",
+        author: "Author Name",
+        user: user
+      })
+      persianFood = Repo.insert!(%Roots.Cookbook{
+        title: "Persion Food",
+        author: "Author Name",
+        user: user
+      })
 
-      userCookbook = insert(:cookbook)
-      persianFood  = insert(:cookbook)
       results = Cookbook.all()
       assert length(results) == 2
-      assert List.first(results).id == usersCookbook.id
-      assert List.last(results).id == persianFood.id
+      assert List.first(results).id == persianFood.id
+      assert List.last(results).id == usersCookbook.id
     end
   end
 end
