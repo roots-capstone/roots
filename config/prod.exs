9180e586342f5a568c2d205961c1aacdfdb61f12
“Neeru <neeram85@gmail.com>
2020-10-30 10:38:33 -0600

    Added server:true to config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :roots, RootsWeb.Endpoint,
   url: [scheme: "https", host: "glacial-falls-21490.herokuapp.com", port: 443],
+  server: true,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
