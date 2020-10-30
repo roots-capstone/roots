aaf0dfa0c80df1cfa139a6cce6f1b108764dfb8f
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-10-29 22:50:29 -0600

    Updata seed file. Add recipes

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -46,59 +46,115 @@ horacio = Repo.insert!(%Roots.User{
 neerus_cookbook = Repo.insert!(%Roots.Cookbook{
   title: "Neeru's Cookbook",
   author: "Neeru",
-  user: neeru
+  user_id: neeru.id
 })
 indian_food = Repo.insert!(%Roots.Cookbook{
   title: "Indian Food",
   author: "Neeru",
-  user: neeru
+  user_id: neeru.id
 })
 ryans_cookbook = Repo.insert!(%Roots.Cookbook{
   title: "Ryan's Cookbook",
   author: "Ryan",
-  user: ryan
+  user_id: ryan.id
 })
 michael_alexs_cookbook = Repo.insert!(%Roots.Cookbook{
   title: "MichaelAlex's Cookbook",
   author: "MichaelAlex",
-  user: michaelAlex
+  user_id: michaelAlex.id
 })
 jessyes_cookbook = Repo.insert!(%Roots.Cookbook{
   title: "Jessye's Cookbook",
   author: "Jessye",
-  user: jessye
+  user_id: jessye.id
 })
 tylers_cookbook = Repo.insert!(%Roots.Cookbook{
   title: "Tyler's Cookbook",
   author: "Tyler",
-  user: tyler
+  user_id: tyler.id
 })
 horacios_cookbook = Repo.insert!(%Roots.Cookbook{
   title: "Horacio's Cookbook",
   author: "Horacio",
-  user: horacio
+  user_id: horacio.id
 })
 
 
 # RECIPES
-beed_potato_stew = Repo.insert!(%Roots.Recipe{
+beef_potato_stew = Repo.insert!(%Roots.Recipe{
    description: "A hearty beef anmd potato stew",
    instructions: "Cook in a crockpot",
    title: "Campfire Stew",
    author: "Michael Alex",
-   cookbook: michael_alexs_cookbook
+   cookbook_id: michael_alexs_cookbook.id
 })
 baked_chicken = Repo.insert!(%Roots.Recipe{
    description: "Lemon=garlic baked chicken cooked with potatoes",
    instructions: "Bake at 375F",
    title: "Greek Chicken and Potatoes",
    author: "Michael Alex",
-   cookbook: michael_alexs_cookbook
+   cookbook_id: michael_alexs_cookbook.id
 })
 french_dish = Repo.insert!(%Roots.Recipe{
    description: "A classic French dish",
    instructions: "Cook the beef and carrots in red wine",
    title: "Beef Bourguinon",
    author: "Michael Alex",
-   cookbook: michael_alexs_cookbook
+   cookbook_id: michael_alexs_cookbook.id
+})
+
+# INGREDIENTS
+beef = Repo.insert!(%Roots.Ingredient{
+  name: "beef",
+  amount: 2.0,
+  unit: "lbs",
+  recipe_id: beef_potato_stew.id
+})
+potato = Repo.insert!(%Roots.Ingredient{
+  name: "potato",
+  amount: 3.0,
+  unit: "lbs",
+  recipe_id: beef_potato_stew.id
+})
+chicken = Repo.insert!(%Roots.Ingredient{
+  name: "chicken",
+  amount: 2.0,
+  unit: "lbs",
+  recipe_id: baked_chicken.id
+})
+potato = Repo.insert!(%Roots.Ingredient{
+  name: "potato",
+  amount: 3.0,
+  unit: "lbs",
+  recipe_id: baked_chicken.id
+})
+lemon = Repo.insert!(%Roots.Ingredient{
+  name: "lemon",
+  amount: 1.0,
+  unit: "whole lemon",
+  recipe_id: baked_chicken.id
+})
+garlic = Repo.insert!(%Roots.Ingredient{
+  name: "garlic",
+  amount: 3.0,
+  unit: "cloves",
+  recipe_id: baked_chicken.id
+})
+beef = Repo.insert!(%Roots.Ingredient{
+  name: "beef",
+  amount: 2.0,
+  unit: "lbs",
+  recipe_id: french_dish.id
+})
+potato = Repo.insert!(%Roots.Ingredient{
+  name: "potato",
+  amount: 3.0,
+  unit: "lbs",
+  recipe_id: french_dish.id
+})
+red_wine = Repo.insert!(%Roots.Ingredient{
+  name: "red wine",
+  amount: 7.0,
+  unit: "oz",
+  recipe_id: french_dish.id
 })
