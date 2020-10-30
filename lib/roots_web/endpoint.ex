4d4ce5a76b0d41342510ae4652df987ee21923f2
“Neeru <neeram85@gmail.com>
2020-10-30 10:53:18 -0600

    comment out things in the endpoint file

--- lib/roots_web/endpoint.ex
+++ lib/roots_web/endpoint.ex
@@ -4,27 +4,27 @@ defmodule RootsWeb.Endpoint do
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  @session_options [
-    store: :cookie,
-    key: "_roots_key",
-    signing_salt: "l8eEWfz6"
-  ]
-
-  socket "/socket", RootsWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  # @session_options [
+  #   store: :cookie,
+  #   key: "_roots_key",
+  #   signing_salt: "l8eEWfz6"
+  # ]
+  #
+  # socket "/socket", RootsWeb.UserSocket,
+  #   websocket: true,
+  #   longpoll: false
+  #
+  # socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :roots,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+  # plug Plug.Static,
+  #   at: "/",
+  #   from: :roots,
+  #   gzip: false,
+  #   only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
@@ -33,9 +33,9 @@ defmodule RootsWeb.Endpoint do
     plug Phoenix.Ecto.CheckRepoStatus, otp_app: :roots
   end
 
-  plug Phoenix.LiveDashboard.RequestLogger,
-    param_key: "request_logger",
-    cookie_key: "request_logger"
+  # plug Phoenix.LiveDashboard.RequestLogger,
+  #   param_key: "request_logger",
+  #   cookie_key: "request_logger"
 
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
@@ -47,6 +47,6 @@ defmodule RootsWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
-  plug Plug.Session, @session_options
+  # plug Plug.Session, @session_options
   plug RootsWeb.Router
 end
