62bf698209730190ee327a43bff67a81a13167ff
“Neeru <neeram85@gmail.com>
2020-11-01 10:22:13 -0700

    Write a test to check show method in the user resolver

---
+++ test/roots_web/resolvers/user_resolver_test.exs
@@ -0,0 +1,29 @@
+defmodule RootsWeb.UserResolverTest do
+  use RootsWeb.ConnCase
+  require IEx
+
+  alias RootsWeb.Resolvers.UserResolver
+  alias Roots.Repo
+
+  describe "#show" do
+    test "returns specific user" do
+      neeru =
+        Repo.insert!(%Roots.User{
+          name: "Neeru",
+          email: "neeru@roots.com"
+        })
+
+      persianFood =
+        Repo.insert!(%Roots.Cookbook{
+          title: "Persion Food",
+          author: "Author Name",
+          user: user
+        })
+
+      {:ok, found} = UserResolver.show(nil, %{id: neeru.id}, nil)
+      assert found.id == neeru.id
+      assert found.name == neeru.name
+      assert found.email == neeru.email
+    end
+  end
+end
