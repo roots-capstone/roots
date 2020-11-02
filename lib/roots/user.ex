c3b4968590c8e2dec9a45c01912039595752b038
“Neeru <neeram85@gmail.com>
2020-11-02 12:17:49 -0700

    Added preloaded relationships to the get_user query

--- lib/roots/user.ex
+++ lib/roots/user.ex
@@ -1,7 +1,7 @@
 defmodule Roots.User do
   use Roots.Model
 
-  alias Roots.{Repo, User, Cookbook}
+  alias Roots.{Repo, User, Cookbook, Recipe}
 
   schema "users" do
     field :email, :string
@@ -19,6 +19,23 @@ defmodule Roots.User do
         preload: :user
 
     Repo.all(query)
+  end
+
+  def get_user(id) do
+    user =
+      Roots.Repo.one(
+        from user in Roots.User,
+          where: user.id == ^id,
+          left_join: cookbook in assoc(user, :cookbook),
+          left_join: recipes in assoc(cookbook, :recipes),
+          left_join: ingredients in assoc(recipes, :ingredients),
+          preload: [
+            cookbook: {cookbook, recipes: recipes},
+            recipe: {recipes, ingredients: ingredients}
+          ]
+      )
+
+    Roots.Repo.one()
   end
 
   @doc false
